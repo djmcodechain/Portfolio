1f1d7ca0c91377b82c01194200f785ef82fb9c6a
djmcodechain <209768261+djmcodechain@users.noreply.github.com>
2025-10-08 22:25:48 +0100

    fix(main.go): fix connection to the index handler
    Signed-off-by: djmcodechain <209768261+djmcodechain@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"danieljmanning/api"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", api.IndexHandler)
+
+	// Serve Static Files
+	fs := http.FileServer(http.Dir("./assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	println("Listening on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
