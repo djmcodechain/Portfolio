b78fcabb1147c82ff65401e9a90f25f08e207b44
djmcodechain <209768261+djmcodechain@users.noreply.github.com>
2025-10-09 11:14:43 +0100

    refac(routes.go): add copyright and dev comments; update routes to return serve Mux
    Signed-off-by: djmcodechain <209768261+djmcodechain@users.noreply.github.com>

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,14 +1,28 @@
 package routes
 
+// Path: backend/cmd/main.go
+// Author: Daniel J. Manning
+// GitHub: https://github.com/djmcodechain/Go-Portfolio
+
 import (
+	// import packages
 	"danieljmanning/backend/handlers"
 	"net/http"
 )
 
-func Routes() {
-	Mux := http.NewServeMux()
+func Routes() *http.ServeMux {
+	// Create a new Mux
+	var Mux = http.NewServeMux()
 
+	// The routes to pages
 	Mux.HandleFunc("/", handlers.IndexHandler)
 
-	http.ListenAndServe(":8080", Mux)
+	// Return Mux to allow it to be served in main.go
+	return Mux
 }
+
+// GNU Public
+// Copyright (c) 2025 Daniel J. Manning
+// Created: Sun, 05 Oct 2025
+//
+// License: GNU Public (See LICENSE file in repository)
