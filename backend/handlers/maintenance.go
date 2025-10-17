e481c3c6521543c8ba78570a99e6e012e3b95a0a
djmcodechain <209768261+djmcodechain@users.noreply.github.com>
2025-10-17 15:21:39 +0100

    chore(maintenance.go): add inital Open Graph Tags
    Signed-off-by: djmcodechain <209768261+djmcodechain@users.noreply.github.com>

--- backend/handlers/maintenance.go
+++ backend/handlers/maintenance.go
@@ -13,4 +13,12 @@ func MaintainenceHandler(w http.ResponseWriter, r *http.Request) {
 		Index:       "index",
 		CSSlink:     "frontend/assets/css/style.css",
 	}
+
+	ogTags := models.OpenGraphTags{
+		Locale:      "GB-en",
+		OGtype:      "website",
+		Title:       meta.Title,
+		Description: meta.Description,
+		URL:         meta.Canonical,
+	}
 }
