85fcdefe9b001b2911c97fae5b6e5ab569064b34
djmcodechain <209768261+djmcodechain@users.noreply.github.com>
2025-10-18 19:19:49 +0100

    tmp: add p tag with text
    Signed-off-by: djmcodechain <209768261+djmcodechain@users.noreply.github.com>

--- backend/handlers/index.go
+++ backend/handlers/index.go
@@ -9,6 +9,9 @@ import (
 // Author: Daniel J. Manning
 // GitHub: https://github.com/djmcodechain/Portfolio
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`
+	<p> Just testing to see if this does response when it has data </p>
+	`))
 }
 
 // GNU Public
