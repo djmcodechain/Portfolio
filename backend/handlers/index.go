0d8b3bce509c340fe1810324179c7fd37732a984
djmcodechain <209768261+djmcodechain@users.noreply.github.com>
2025-10-19 08:58:05 +0100

    chore(index.go): add logging info with context
    Signed-off-by: djmcodechain <209768261+djmcodechain@users.noreply.github.com>

--- backend/handlers/index.go
+++ backend/handlers/index.go
@@ -3,12 +3,16 @@ package handlers
 import (
 	// import packages
 	"net/http"
+
+	"github.com/djmcodechain/Portfolio/backend/logging"
 )
 
 // Path: backend/handlers/index.go
 // Author: Daniel J. Manning
 // GitHub: https://github.com/djmcodechain/Portfolio
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	defer logging.Logger.InfoContext(ctx, "IndexHandler() ~ called")
 	w.Write([]byte(`
 	<p> Just testing to see if this does response when it has data </p>
 	`))
